4d6d6fadb9bff7a24fd048a067ed7ca241431b03
0306241126-svg <0306241126@caothang.edu.vn>
2025-09-04 15:15:42 +0700

    Update bai3.php

--- bai3.php
+++ bai3.php
@@ -4,37 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<head>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-
-        td {
-            width: 60px;
-            height: 60px;
-        }
-
-    </style>
 </head>
-<body>
-
-<?php
-echo "<table border='1'>";
-for ($a = 0; $a < 8; $a++) {
-    echo "<tr>";
-    for ($b = 0; $b < 8; $b++) {
-        // Ô trắng nếu tổng hàng + cột là chẵn
-        $color = ($a + $b) % 2 == 0 ? 'white' : 'black';
-        echo "<td class='$color'></td>";
+<style>
+    td {
+        width: 50px;
+        height: 50px;
     }
-    echo "</tr>";
+</style>
+
+<body>
+<?php
+echo "<table border='1'>";     
+for ($a = 0; $a < 8; $a++) {  
+    echo "<tr>";              
+    for ($b = 0; $b < 8; $b++) {   
+        echo "<td ></td>";
+    }
+    echo "</tr>";             
 }
-echo "</table>";
+echo "</table>";        
 ?>
 
 </body>
